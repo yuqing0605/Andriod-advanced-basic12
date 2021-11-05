980b44fd5e5fc4ffbaab99cf5a15017e7373b781
LinYuChhing <linyuching@gmail.com>
2021-11-05 20:41:12 +0800

    first upload

---
+++ basic12/app/src/main/java/com/example/basic12/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.basic12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Cswitch(View view){
+        ImageView imageView =  findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.b);
+    }
+}
